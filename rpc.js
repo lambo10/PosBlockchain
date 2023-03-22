d6cd57bddd1b4d3143fb1cac4d6b6bea68232aa9
Lambert Nnadi <40530031+lambo10@users.noreply.github.com>
2023-03-22 06:39:16 +0100

    new commit

---
+++ rpc.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import PoSBlockchain from './pos_blockchain.js';
+
+const app = express();
+app.use(bodyParser.json());
+
+const validators = [
+    new Validator('validator1', 100),
+    new Validator('validator2', 150),
+    new Validator('validator3', 200),
+  ];
+  
+  const blockchain = new PoSBlockchain(validators);
+
+  app.post('/jsonrpc', (req, res) => {
+    const { id, method, params } = req.body;
+  
+    if (method === 'getLatestBlock') {
+      const result = blockchain.getLatestBlock();
+      res.json({ jsonrpc: '2.0', id, result });
+    } else if (method === 'addTransaction') {
+      try {
+        const transaction = new Transaction(params.inputs, params.outputs);
+        blockchain.addTransaction(transaction);
+        res.json({ jsonrpc: '2.0', id, result: 'Transaction added' });
+      } catch (error) {
+        res.json({ jsonrpc: '2.0', id, error: { code: -32603, message: 'Invalid transaction' } });
+      }
+    } else {
+      res.json({ jsonrpc: '2.0', id, error: { code: -32601, message: 'Method not found' } });
+    }
+  });
+
+  const PORT = process.env.PORT || 3590;
+app.listen(PORT, () => {
+  console.log(`JSON-RPC server listening on port ${PORT}`);
+});
